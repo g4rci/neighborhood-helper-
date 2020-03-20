4ce432b193cc36b65c5f790692bca56157a7a118
Lou08 <l.casals86@gmail.com>
2020-03-20 17:35:46 +0100

    signup ok

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   direction: {type: String, required: true},
-  picture: String,
+  picture: { type: String, default: "https://banner2.cleanpng.com/20180404/djw/kisspng-computer-icons-users-group-internet-forum-user-avatar-5ac45a991206f5.9866985115228176890738.jpg"}
 });
 
 userSchema.set('timestamps', true);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,72 @@
 var express = require('express');
 var router = express.Router();
-var bcrypt = require("bcryptjs");
+var bcrypt = require("bcrypt");
 var bcryptSalt = 10;
 
-const User = require("../models/User.js");
+const User = require("../models/user");
 
+router.get('/signup', (req,res,next)=>{
+    res.render ('auth/signup');
+})
+
+router.post('/signup', (req,res,next)=>{
+
+    const {name, email, password, repeatpassword, direction} = req.body;
+    if(!name || !email || !password){
+        res.render('auth/signup', {errormessage:'Please complete all the fields'} );
+        return;
+    }
+    if(password !== repeatpassword){
+        res.render('auth/signup', {errormessage:'Password do not match'} );
+        return;
+    }
+    User 
+    .findOne({email})
+    .then (user=>{
+        if(user){
+            res.render('auth/signup', {errormessage: 'The email already exist'});
+            return;
+        }
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashpass = bcrypt.hashSync(password, salt);
+        User.create({name, email, 'password':hashpass, direction})
+        .then((user)=>{
+            req.session.currentUser = user;
+            res.render('index')
+        })
+        .catch (err => console.log ('Error creating user: ' +err));
+    })
+    .catch (err => console.log ('Error finding the user on DataBase: ' +err))
+})
+
+router.get("/login", (req, res, next)=>{
+    // renderizar las vistas de login
+    res.render("log-in.hbs")
+})
+router.post("/login", (req, res, next)=>{
+    const { username, password } = req.body;
+    if(username === "" || password === ""){
+        res.render("log-in", {errorMessage: "Please, fill all the fields"})
+    }
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                res.render("log-in", {
+                    errorMessage: "There is no user with that username"
+                })
+            }
+            //bcrypt.compareSync(contraseñaNormal, contraseñaHassheda)
+            if (bcrypt.compareSync(password, user.password)) {
+                req.session.currentUser = user;
+                res.redirect("/priv/")
+            }
+            else {
+                res.render("log-in", {
+                    errorMessage: "Incorrect password"
+                })
+            }
+        })
+        .catch(err => console.log("error finding the user: " + err))
+})
+
+module.exports = router;
