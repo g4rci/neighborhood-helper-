93059ef55cd6d42bc51774b411cf4e6ee4dda836
Lou08 <l.casals86@gmail.com>
2020-03-23 12:43:17 +0100

    imprime el profile

--- routes/priv.js
+++ routes/priv.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const userIsLoggedIn = require("../middlewares/auth-mid").userIsLoggedIn
-const Task = require ('../models/task');
-const User = require ('../models/user')
+const Task = require('../models/task');
+const User = require('../models/user')
 
-router.use((req, res, next)=> userIsLoggedIn(req, res, next));
+router.use((req, res, next) => userIsLoggedIn(req, res, next));
 
 //MIDDLEWARE A SACO
 // router.use((req,res,next) =>{
@@ -17,29 +17,40 @@ router.use((req, res, next)=> userIsLoggedIn(req, res, next));
 // })
 //********************************* */
 
-router.get("/create-task", (req, res, next) =>{
+router.get("/create-task", (req, res, next) => {
     //el get solo tiene que renderizar la vista
     console.log('hola')
     res.render("private/create-task")
 })
 
-router.post("/create-task", async (req, res, next)=>{
-    const {name, description} = req.body
-    
-    const newTask = await Task.create({name, description})
+router.post("/create-task", async (req, res, next) => {
+    const { name, description } = req.body
+
+    const newTask = await Task.create({ name, description })
     console.log('new task is :', newTask);
-    
+
     const userId = req.session.currentUser._id
-    
-    await User.updateOne({_id: userId}, { $push: {tasks: newTask._id} })
-    
-    res.render("private/create-task", {message:"created successfully"})
-    
+
+    await User.updateOne({ _id: userId }, { $push: { tasks: newTask._id } })
+
+    res.render("private/create-task", { message: "created successfully" })
+
 })
 
 
-router.get('/profile', (req, res ,nex) => {
-    console.log('estamos en profile')
-    res.render('private/profile')
+router.get('/profile', (req, res, nex) => {
+    const userId = req.session.currentUser._id
+    console.log('usuario', userId);
+    User
+        .findById(userId)
+        .populate("tasks")
+        .then(user => {
+             console.log(user);
+            res.render('private/profile', {user})
+        }
+        )
 })
+// router.get('/users', (req, res, nex) => {
+
+// });
 module.exports = router;
