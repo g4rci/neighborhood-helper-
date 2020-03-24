d0c2eadb5c4b4d771be18baddbb0f28ed77c2c3b
Lou08 <l.casals86@gmail.com>
2020-03-24 17:06:10 +0100

    en proceso

--- models/user.js
+++ models/user.js
@@ -7,8 +7,8 @@ const userSchema = new Schema({
   password: {type: String, required: true},
   direction: {type: String, required: true},
   picture: { type: String, default: "https://banner2.cleanpng.com/20180404/djw/kisspng-computer-icons-users-group-internet-forum-user-avatar-5ac45a991206f5.9866985115228176890738.jpg"},
-  tasks:[{type: Schema.Types.ObjectId, ref:'Task'}],
-  requests: [{type: Schema.Types.ObjectId, ref:'Task'}]
+  tasks:[{type: Schema.Types.ObjectId, ref:'Task'}],// las que quiero que me hagan
+  requests: [{type: Schema.Types.ObjectId, ref:'Task'}] // las que yo hago
 });
 
 userSchema.set('timestamps', true);
