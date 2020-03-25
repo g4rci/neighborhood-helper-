adf67fa4c8fafb2de9221512a7a5acd0d8fa5809
M.A.Garcia <m.angel.gf04.com>
2020-03-25 17:52:15 +0100

    a ver si asi si

--- app.js
+++ app.js
@@ -19,30 +19,30 @@ const privRouter = require('./routes/priv');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true }, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+.then(x => {
+  console.log(
+    `Connected to Mongo! Database name: "${x.connections[0].name}"`
+  );
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err);
+});
 
-  app.use(session({
-    secret: 'hello',
-    resave: true,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 },
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl:  24 * 60 * 60 // 1 day
-    })
-  }));
+app.use(session({
+  secret: 'hello',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl:  24 * 60 * 60 // 1 day
+  })
+}));
 
 
   app.use(function(req, res, next) {
